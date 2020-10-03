365c413c388e4e33c7567c8e4f2a93a1ac791af6
Sergey <52304443+c29r3@users.noreply.github.com>
2020-10-03 06:04:43 +0300

    Create run_docker.sh

---
+++ run_docker.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+docker run -d \
+  --restart always \
+  --entrypoint /usr/bin/nym-mixnode \
+  --volume /root/nym/.nym/:/root/.nym \
+  --name nym-1 \
+  -p 1789:1789 \
+  --log-opt max-size=100m \
+  --log-opt max-file=3 \
+  c29r3/nym-mixnode:v0.8.1 \
+  run --id 1
