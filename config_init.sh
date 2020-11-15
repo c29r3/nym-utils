a94beaf0d5f8cf5a4c5103fb543a8a57b0b2fe50
Sergey <52304443+c29r3@users.noreply.github.com>
2020-11-15 18:23:22 +0300

    Update config_init.sh

--- config_init.sh
+++ config_init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-mkdir nym; cd nym;\
-wget https://github.com/nymtech/nym/releases/download/v0.8.1/nym-mixnode_linux_x86_64 \
+
+mkdir $HOME/nym; cd $HOME/nym;\
+wget https://github.com/nymtech/nym/releases/download/v0.9.1/nym-mixnode_linux_x86_64 \
 && chmod u+x nym-mixnode_linux_x86_64 \
-&& ~/nym/nym-mixnode_linux_x86_64 init --id 1 --host $(curl 2ip.ru) --location $(curl -s ipinfo.io/$(curl -s 2ip.ru) | jq -r '.city') \
-&& mv ~/.nym ~/nym/
+&& $HOME/nym/nym-mixnode_linux_x86_64 init --id 1 --host $(curl 2ip.ru) --location $(curl -s ipinfo.io/$(curl -s 2ip.ru) | jq -r '.city') \
+&& mv $HOME/.nym $HOME/nym/
