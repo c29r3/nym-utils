a9d264a75376bfe730ba362546a609b8a2fec8b7
Sergey <52304443+c29r3@users.noreply.github.com>
2020-11-16 00:27:47 +0300

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,6 @@ docker run -d \
   --volume $HOME/.nym/:/root/.nym \
   --network host \
   --name nym \
-  --network host \
   --log-opt max-size=100m \
   --log-opt max-file=3 \
   c29r3/nym-mixnode:latest \
